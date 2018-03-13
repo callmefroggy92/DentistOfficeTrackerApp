dc4b97a0e564ea2ac299bd64d09123846ec01021
Isaac Sanchez <35980824+isaako1@users.noreply.github.com>
2018-03-12 23:12:39 -0600

    Adding TeethDiagram.java draft Adding activity_teeth_diagram.xml Draft and image for teeth diagram
    Adding changes to Strings

---
+++ app/src/main/java/com/example/froggy/dentistofficetracker/TeethDiagram.java
@@ -0,0 +1,56 @@
+package com.example.froggy.dentistofficetracker;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class TeethDiagram extends Activity {// this change lets the requestWindowFeature(Window.FEATURE_NO_TITLE) to work
+    //public class TeethDiagram extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // this removes the title
+        setContentView(R.layout.activity_teeth_diagram);
+
+
+        // PIECE Spinner
+        Spinner myPieceSpinner =(Spinner) findViewById(R.id.spinnerPiecelist);
+        ArrayAdapter<String> myPieceAdapter = new ArrayAdapter<String>(TeethDiagram.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.pieceList));
+
+        myPieceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myPieceSpinner.setAdapter(myPieceAdapter);
+
+
+
+        // FACE Spinner
+        Spinner myFaceSpinner =(Spinner) findViewById(R.id.spinnerFaceList);
+        ArrayAdapter<String> myFaceAdapter = new ArrayAdapter<String>(TeethDiagram.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.faceList));
+
+        myFaceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myFaceSpinner.setAdapter(myFaceAdapter);
+
+
+        // DIAGNOSTICS Spinner
+        Spinner myDiagSpinner =(Spinner) findViewById(R.id.spinnerDiagList);
+        ArrayAdapter<String> myDiagAdapter = new ArrayAdapter<String>(TeethDiagram.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.diagList));
+
+        myDiagAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myDiagSpinner.setAdapter(myDiagAdapter);
+
+
+        // PROCEDURE Spinner
+        Spinner myProcSpinner =(Spinner) findViewById(R.id.spinnerProcList);
+        ArrayAdapter<String> myProcAdapter = new ArrayAdapter<String>(TeethDiagram.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.procList));
+
+        myProcAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myProcSpinner.setAdapter(myProcAdapter);
+
+    }
+}
