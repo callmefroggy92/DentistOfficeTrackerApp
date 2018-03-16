e640beb95fdc9e86842895920d7cf7225be1cd7d
Osvaldo <osvaldofabricio11@hotmail.com>
2018-03-16 15:48:28 -0600

    I've added some Logs d in the Main Activity for the team assignment.

--- app/src/main/java/com/example/froggy/dentistofficetracker/MainActivity.java
+++ app/src/main/java/com/example/froggy/dentistofficetracker/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.froggy.dentistofficetracker;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.firebase.database.DataSnapshot;
@@ -26,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference myRef;
     private SharedPreferences prefs;
-
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,114 +42,163 @@ public class MainActivity extends AppCompatActivity {
             username_text.setText(prefs.getString("username", ""));
         }
 
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("Loading");
+        progressDialog.setMessage("Processing");
+        progressDialog.setCancelable(false);
+
+        //Login dialog
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                progressDialog.show();
+                new BackgroundJob().execute();
+                Toast.makeText(MainActivity.this, "TASK STARTED", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
     public void onLogin(View view) throws AuthException{
-        EditText username_text = findViewById(R.id.username_text);
-        final String username = username_text.getText().toString();
-        EditText password_text = findViewById(R.id.password_text);
-        final String password = password_text.getText().toString();
 
-        // Create Firebase and reference
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        myRef = firebaseDatabase.getReference();
 
-        // EventListener retrieves values from the database and updates the application if anything changes
-        ValueEventListener listener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // First, check to see if the given username exists in the database
-                if (dataSnapshot.hasChild(username)) {
-                    Gson gson = new Gson();
 
-                    // create a new user object and fill it with login information
-                    user = new User();
-                    user.setSalt(dataSnapshot.child(username).child("salt").getValue().toString());
-                    user.setUsername(dataSnapshot.child(username).child("username").getValue().toString());
-                    user.setKey(dataSnapshot.child(username).child("key").getValue().toString());
-                    user.setHash(dataSnapshot.child(username).child("hash").getValue().toString());
+    }
 
-                    //My first TAG
-                    Log.d(TAG, "My First TAG");
+    private class BackgroundJob extends AsyncTask<Void,Void, Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
 
-                    // The type determines where the user is sent after login
-                    String userType = dataSnapshot.child(username).child("type").getValue().toString();
-                    if (userType.equals("Admin"))
-                        user.type = UserType.Admin;
-                    else if (userType.equals("Patient"))
-                        user.type = UserType.Patient;
-                    else if (userType.equals("Dentist"))
-                        user.type = UserType.Dentist;
-                    else {
-                        // If it has no type, it is not vaild
-                        Toast.makeText(getApplicationContext(), "Invalid user file!", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-                    // Get the type of the user
-                    Log.d(TAG, "name " +  user.getType());
 
-                    // Get the name of the user
-                    Log.d(TAG, "name " + user.getName());
-                    user.setPassword(password);
-                    try {
-                        // Password verification
-                        if (LoginController.verifyPassword(user)) {
-                            Toast.makeText(getApplicationContext(), "Login Succesful!", Toast.LENGTH_SHORT).show();
+            EditText username_text = findViewById(R.id.username_text);
+            final String username = username_text.getText().toString();
+            EditText password_text = findViewById(R.id.password_text);
+            final String password = password_text.getText().toString();
 
-                            // If login was successful, the username is saved in sharedpreferences
-                            // until the next login
-                            SharedPreferences.Editor editor = prefs.edit();
-                            editor.putString("username", user.getUsername());
-                            editor.apply();
 
-                            // Sends the user to different activity depending on the usertype
-                            if (user.getType() == UserType.Admin) {
-                                //Intent i = new Intent(getApplicationContext(), activity_admin.class);
-                                Intent i = new Intent(getApplicationContext(), NavigationDrawer.class);
-                                i.putExtra("username", user.getUsername());
-                                i.putExtra("key", user.getDecKey());
-                                //startActivity(i);
-                                startActivity(i);
-                            } else if (user.getType() == UserType.Patient) {
-                               // Intent i = new Intent(getApplicationContext(), activity_patient.class);
-                                Intent i = new Intent(getApplicationContext(), NavigationDrawer.class);
-                                i.putExtra("username", user.getUsername());
-                                i.putExtra("key", user.getDecKey());
-                               // startActivity(i);
-                                startActivity(i);
-                            } else if (user.getType() == UserType.Dentist) {
-                                Intent i = new Intent(getApplicationContext(), NavigationDrawer.class);
-                               // Intent i = new Intent(getApplicationContext(), activity_dentist.class);
-                                i.putExtra("username", user.getUsername());
-                                i.putExtra("key", user.getDecKey());
-                                startActivity(i);
-                            } else {
-                                throw new AuthException("Error: User file inaccessible");
-                            }
+            // Create Firebase and reference
+            firebaseDatabase = FirebaseDatabase.getInstance();
+            myRef = firebaseDatabase.getReference();
 
-                        } else {
-                            // If the password is wrong. . .
-                            Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_SHORT).show();
+            // EventListener retrieves values from the database and updates the application if anything changes
+            ValueEventListener listener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // First, check to see if the given username exists in the database
+                    if (dataSnapshot.hasChild(username)) {
+                        Gson gson = new Gson();
+
+                        // create a new user object and fill it with login information
+                        user = new User();
+                        user.setSalt(dataSnapshot.child(username).child("salt").getValue().toString());
+                        user.setUsername(dataSnapshot.child(username).child("username").getValue().toString());
+                        user.setKey(dataSnapshot.child(username).child("key").getValue().toString());
+                        user.setHash(dataSnapshot.child(username).child("hash").getValue().toString());
+
+                        //My first TAG
+                        Log.d(TAG, "My First TAG");
+
+                        // The type determines where the user is sent after login
+                        String userType = dataSnapshot.child(username).child("type").getValue().toString();
+                        if (userType.equals("Admin"))
+                            user.type = UserType.Admin;
+                        else if (userType.equals("Patient"))
+                            user.type = UserType.Patient;
+                        else if (userType.equals("Dentist"))
+                            user.type = UserType.Dentist;
+                        else {
+                            // If it has no type, it is not vaild
+                            Toast.makeText(getApplicationContext(), "Invalid user file!", Toast.LENGTH_SHORT).show();
+                            return;
                         }
-                    } catch (Exception e) {
-                        Log.e("LoginController: ", e.getMessage());
-                    }
-                } else
-                    // If the username is wrong. . .
-                    Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("onCancelled: ", "Failed to read value.", error.toException());
-            }
-        };
-        // Add the ValueEventListener to login the user.
-        // This function only retrieves the data once instead of continuously
-        // looking for updates.
-        myRef.addListenerForSingleValueEvent(listener);
+                        // Get the type of the user
+                        Log.d(TAG, "name " +  user.getType());
 
+                        // Get the name of the user
+                        Log.d(TAG, "name " + user.getName());
+
+
+                        user.setPassword(password);
+                        try {
+
+                            // Password verification
+                            if (LoginController.verifyPassword(user)) {
+                                Toast.makeText(getApplicationContext(), "Login Succesful!", Toast.LENGTH_SHORT).show();
+
+                                // If login was successful, the username is saved in sharedpreferences
+                                // until the next login
+                                SharedPreferences.Editor editor = prefs.edit();
+                                editor.putString("username", user.getUsername());
+                                editor.apply();
+
+
+                                // Sends the user to different activity depending on the usertype
+                                if (user.getType() == UserType.Admin) {
+
+                                    //Intent i = new Intent(getApplicationContext(), activity_admin.class);
+                                    Intent i = new Intent(getApplicationContext(), NavigationDrawer.class);
+                                    i.putExtra("username", user.getUsername());
+                                    i.putExtra("key", user.getDecKey());
+                                    //startActivity(i);
+                                    startActivity(i);
+                                } else if (user.getType() == UserType.Patient) {
+                                    // Intent i = new Intent(getApplicationContext(), activity_patient.class);
+                                    Intent i = new Intent(getApplicationContext(), NavigationDrawer.class);
+                                    i.putExtra("username", user.getUsername());
+                                    i.putExtra("key", user.getDecKey());
+                                    // startActivity(i);
+                                    startActivity(i);
+                                } else if (user.getType() == UserType.Dentist) {
+                                    Intent i = new Intent(getApplicationContext(), NavigationDrawer.class);
+                                    // Intent i = new Intent(getApplicationContext(), activity_dentist.class);
+                                    i.putExtra("username", user.getUsername());
+                                    i.putExtra("key", user.getDecKey());
+                                    startActivity(i);
+                                } else {
+                                    throw new AuthException("Error: User file inaccessible");
+                                }
+
+                            } else {
+                                // If the password is wrong. . .
+                                Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (Exception e) {
+                            Log.e("LoginController: ", e.getMessage());
+                        }
+                    } else
+                        // If the username is wrong. . .
+                        Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w("onCancelled: ", "Failed to read value.", error.toException());
+                }
+            };
+            // Add the ValueEventListener to login the user.
+            // This function only retrieves the data once instead of continuously
+            // looking for updates.
+            myRef.addListenerForSingleValueEvent(listener);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            //super.onPostExecute(aVoid);
+            Toast.makeText(MainActivity.this, "STOPPED", Toast.LENGTH_SHORT).show();
+            progressDialog.cancel();
+        }
     }
 }
