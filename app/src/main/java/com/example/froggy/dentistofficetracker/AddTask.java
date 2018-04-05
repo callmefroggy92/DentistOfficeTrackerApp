ef75047a2a4f97f97652b1e0fdef4fff121594d5
callmefroggy92 <froggy1992@protonmail.com>
2018-04-04 20:48:17 -0400

    Began adding notifications, added Dentagram button to the View Patient Info activity

--- app/src/main/java/com/example/froggy/dentistofficetracker/AddTask.java
+++ app/src/main/java/com/example/froggy/dentistofficetracker/AddTask.java
@@ -1,6 +1,11 @@
 package com.example.froggy.dentistofficetracker;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +21,7 @@ import com.google.firebase.database.ValueEventListener;
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * This class is designed to create a new task and
@@ -32,6 +38,9 @@ public class AddTask extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     String text_date;
+
+    public static final String ANDROID_CHANNEL_NAME = "ANDROID CHANNEL";
+    public static final String ANDROID_CHANNEL_ID = "com.example.froggy.dentistofficetracker";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,10 +88,34 @@ public class AddTask extends AppCompatActivity {
         DatabaseReference newOne = myRef.push();
         newOne.child("task").setValue(editTask.getText().toString());
         newOne.child("time").setValue(editTime.getText().toString());
+
+        addNotification();
+
       //  myRef.child("task").push();
        // myRef.child("task").setValue(editTask.getText().toString() + " Time: " + editTime.getText().toString());
       //  myRef.child("time").push();
        // myRef.child("time").setValue(editTime.getText().toString());
 
+    }
+
+    private void addNotification(){
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, ANDROID_CHANNEL_ID)
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentTitle("Dentist Tracker")
+                        .setContentText("This is a reminder for your appointment!");
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID,
+                    ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(androidChannel);
+        }
+
+        manager.notify(0, builder.build());
     }
 }
